37dfca9e8acffa8856207f5486c1dfe69bc829ff
Vadzim <notebook>
2020-02-18 19:11:04 +0300

    creade Profile

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
 
 namespace technical_verbs
 {
@@ -22,7 +24,7 @@ namespace technical_verbs
 
     public partial class MainWindow : Window
     {
-        
+
         string answer;
         bool start = false;
 
@@ -33,7 +35,7 @@ namespace technical_verbs
         public MainWindow()
         {
             InitializeComponent();
-            buttons1 = new List<Button> { butAnswer1, butAnswer2, butAnswer3, butAnswer4, butAnswer5};
+            buttons1 = new List<Button> { butAnswer1, butAnswer2, butAnswer3, butAnswer4, butAnswer5 };
             List<TextBlock> ansverMistekeAndCorect = new List<TextBlock> { textMisteke, textСorrectAnswer };
         }
 
@@ -43,59 +45,61 @@ namespace technical_verbs
 
         private void butAnswer_Click(object sender, RoutedEventArgs e)
         {
-                if (((Button)sender).Name == butStart.Name)
+            if (((Button)sender).Name == butStart.Name)
+            {
+                if (!start)
                 {
-                    if (!start)
+                    start = true;
+                    butStart.Content = "Next";
+                    textUserName.IsEnabled = false;
+                }
+
+                ButtonContext();
+                butStart.IsEnabled = false;
+                ((Button)sender).Background = Brushes.Red;
+
+            }
+            else
+            {
+                if (start)
+                {
+                    if (((Button)sender).Content.ToString() == answer)
                     {
-                        start = true;
-                        butStart.Content = "Next";
+                        ((Button)sender).Background = Brushes.Green;
+                        butStart.Background = Brushes.Green;
+                        butStart.IsEnabled = true;
+
+                        corectAnswer++;
+
+                        textСorrectAnswer.Text = corectAnswer.ToString();
+
+
                     }
+                    else
+                    {
+                        if (!butStart.IsEnabled)
 
-                    ButtonContext();
-                    butStart.IsEnabled = false;
-                    ((Button)sender).Background = Brushes.Red;
-
-                }
-                else
-                {
-                    if (start) {
-                        if (((Button)sender).Content.ToString() == answer)
                         {
-                            ((Button)sender).Background = Brushes.Green;
-                            butStart.Background = Brushes.Green;
-                            butStart.IsEnabled = true;
-                           
-                            corectAnswer++;
-
-                            textСorrectAnswer.Text = corectAnswer.ToString();
-
-
-                        }
-                        else
-                        {
-                            if (!butStart.IsEnabled)
-
-                            {
-                                ((Button)sender).Background = Brushes.Red;
-                                mistakeAnswer++;
-                                textMisteke.Text = mistakeAnswer.ToString();
-                            }
+                            ((Button)sender).Background = Brushes.Red;
+                            mistakeAnswer++;
+                            textMisteke.Text = mistakeAnswer.ToString();
                         }
                     }
                 }
-            }        
+            }
+        }
 
         public void ButtonContext()
         {
             int x;
-
+            
             int[] randoms = new int[5];
             int temp = 0;
             Random rnd = new Random();
-            randoms[0] = rnd.Next(0, 10);
+            randoms[0] = rnd.Next(0, Data.data.Count-1);
             for (int i = 1; i < 5; i++)
             {
-                temp = rnd.Next(0, 5);
+                temp = rnd.Next(0, Data.data.Count - 1);
 
                 for (int j = 0; j <= i; j++)
                 {
@@ -118,12 +122,65 @@ namespace technical_verbs
 
             }
             x = randoms[rnd.Next(0, 4)];
-           
+
             answer = Data.data[x][0];
             textRus.Text = Data.data[x][1];
 
         }
-    }
 
+        private void credeUser_Click(object sender, RoutedEventArgs e)
+        {
+            if (!start)
+            {
+                textUserName.IsEnabled = true;
+               
+            }
+        }
+
+        private void saveFile_Click(object sender, RoutedEventArgs e)
+        {
+            string mydocu = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\tv\";
+            if (!Directory.Exists(mydocu))
+                Directory.CreateDirectory(mydocu);
+
+            mydocu += $"{textUserName.Text}.vbdata";
+            StreamWriter sr = File.CreateText(mydocu);
+
+            sr.WriteLine($"{textUserName.Text}");
+            sr.WriteLine($"{corectAnswer}");
+            sr.WriteLine($"{mistakeAnswer}");
+
+            sr.Close();
+            MessageBox.Show($"{textUserName.Text}.vbdata сохранен");
+
+        }
+
+        private void openFile_Click(object sender, RoutedEventArgs e)
+        {
+            string filePath;
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "vbdata files(*.vbdata) | *.vbdata";
+            ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\tv\";
+            if (ofd.ShowDialog()== true)
+            {
+                filePath = ofd.FileName;
+                StreamReader sr = File.OpenText(filePath);
+                try
+                {
+                    textUserName.Text = sr.ReadLine();
+                    corectAnswer = int.Parse(sr.ReadLine());
+                    mistakeAnswer = int.Parse(sr.ReadLine());
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Фаил поврежден");
+                }
+
+            }
+            
+        }
+    }
 }
+
+
 
